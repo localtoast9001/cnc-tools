f648f5ebb8a9c20d0a14b786d5a861074578a24b
Jon Rowlett <motherplucker@gmail.com>
2024-12-07 10:37:32 -0800

    Boilerplate project setup.

---
+++ Common.GCode/PositioningMode.cs
@@ -0,0 +1,22 @@
+//-----------------------------------------------------------------------
+// <copyright file="PositioningMode.cs" company="Jon Rowlett">
+//     Copyright (C) Jon Rowlett. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Common.GCode;
+
+/// <summary>
+/// Describes how the machine should interpret positioning in the commands.
+/// </summary>
+public enum PositioningMode
+{
+    /// <summary>
+    /// Coordinates are relative to the current position.
+    /// </summary>
+    Relative = 0,
+
+    /// <summary>
+    /// Coordinates are absolute positions.
+    /// </summary>
+    Absolute,
+}
